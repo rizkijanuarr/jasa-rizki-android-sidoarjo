db50e39b204396fe2cc607a15d6d5166f53c1d45
rizkijanuari <rizkijanuar.work@gmail.com>
2026-02-25 19:14:08 +0700

    FEAT[.]: .

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/jasa-bikin-aplikasi-android-sidoarjo',
+  // base: '/jasa-bikin-aplikasi-android-sidoarjo',
   vite: {
     plugins: [tailwindcss()]
   }
